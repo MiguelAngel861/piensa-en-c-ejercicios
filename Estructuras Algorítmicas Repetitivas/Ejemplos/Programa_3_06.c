39730e0d2789904adcac8a3b16c747fa2619f811
MiguelAngel861 <miguelangelscott861@gmail.com>
2024-09-29 10:48:43 -0600

    Add files via upload

---
+++ Estructuras Algorítmicas Repetitivas/Ejemplos/Programa_3_06.c
@@ -0,0 +1,33 @@
+/* Nómina de profesores.
+
+El programa, al recibir como datos los salarios de los profesores de una
+➥universidad, obtiene la nómina y el promedio de los salarios.
+
+I: variable de tipo entero.
+SAL, NOM y PRO: variables de tipo real. */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int I = 0;
+    float SAL, PRO, NOM = 0;
+
+    printf("Ingrese el salario del profesor:\t");
+    scanf("%f", &SAL);
+    /* Observa que al menos se necesita ingresar el salario de un profesor para que
+    ➥no ocurra un error de ejecución del programa. */
+    
+    do
+    {
+        NOM = NOM + SAL;
+        I = I + 1;
+
+        printf("Ingrese el salario del profesor -0 para terminar- :\t");
+        scanf("%f", &SAL);
+    }
+    while (SAL);
+    PRO = NOM / I;
+
+    printf("\nNómina: %.2f \t Promedio de salarios: %.2f", NOM, PRO);
+}
