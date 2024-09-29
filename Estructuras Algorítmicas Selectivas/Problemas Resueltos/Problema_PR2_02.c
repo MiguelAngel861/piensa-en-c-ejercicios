9a69123a30af75dc43bce6a60f0d02e3376bf2df
MiguelAngel861 <miguelangelscott861@gmail.com>
2024-09-29 10:44:57 -0600

    Add files via upload

---
+++ Estructuras Algorítmicas Selectivas/Problemas Resueltos/Problema_PR2_02.c
@@ -0,0 +1,30 @@
+/* Par, impar o nulo.
+
+NUM: variable de tipo entero. */
+
+#include <stdio.h>
+#include <math.h>
+
+void main(void)
+{
+    int NUM;
+
+    printf("Ingrese el numero: ");
+    scanf("%d", &NUM);
+
+    if (NUM == 0)
+    {
+        printf("\nNulo");
+    }
+    else
+    {
+        if (pow (-1, NUM) > 0)
+        {
+            printf("\nPar");
+        }
+        else
+        {
+            printf("\nImpar");
+        }
+    }
+}
