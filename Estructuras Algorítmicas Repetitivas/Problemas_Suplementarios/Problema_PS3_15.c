b4e81f86e1049c762e3c5c50c76638c00db215b4
MiguelAngel861 <miguelangelscott861@gmail.com>
2024-12-11 09:53:56 -0600

    yap

--- Estructuras Algorítmicas Repetitivas/Problemas_Suplementarios/Problema_PS3_15.c
+++ Estructuras Algorítmicas Repetitivas/Problemas_Suplementarios/Problema_PS3_15.c
@@ -2,5 +2,34 @@
 
 int main(void) 
 {
+        int N1, N2;
+        int aux, resultado;
+
+        printf("Ingrese el primer numero: ");
+        scanf("%d", &N1);
+
+        printf("Ingrese el segundo numero: ");
+        scanf("%d", &N2);
+
+        if (N1 <= 0 || N2 <= 0) {
+                puts("Solo se permiten numero naturales");
+
+                return 1;
+        }
+
+        if (N1 < N2) {
+                aux = N1;
+                N1 = N2;
+                N2 = aux;
+        }
+        
+        while (N2 != 0) {
+                resultado = N1 % N2;
+
+                N1 = N2;
+                N2 = resultado;
+        }
+
+        printf("El MCD es: %d\n", N1);
         return 0;
 }
